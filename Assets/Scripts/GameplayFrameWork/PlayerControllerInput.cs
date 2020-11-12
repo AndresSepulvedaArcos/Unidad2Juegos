8cac6115c41b36818269fc5e0e34003db8321104
andres10 <andres@piola.dev>
2020-11-12 19:45:17 -0300

    drag box

--- Assets/Scripts/GameplayFrameWork/PlayerControllerInput.cs
+++ Assets/Scripts/GameplayFrameWork/PlayerControllerInput.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
+
+[System.Serializable]
+public class AxisMove : UnityEvent<Vector2> { };
 public class PlayerControllerInput : MonoBehaviour
 {
-     
-
+    public AxisMove OnAxisMove;
+    Vector2 targetDirection;
     void ReadInput()
     {
         float horizontalValue = Input.GetAxis("Horizontal");
         float verticalValue = Input.GetAxis("Vertical");
-      
-   
-        GetComponent<CharacterTopDownMovement>().AddMovementInput(horizontalValue, verticalValue);
+
+
+
+        OnAxisMove?.Invoke(targetDirection);
+
 
 
     }
